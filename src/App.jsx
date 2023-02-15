9503ee05a2dcf832ea557792e7e84fc4cab559eb
Neil Merchant <phozee@gmail.com>
2023-02-15 03:57:22 -0700

    basic functionality of adding new appointment

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [appointments, setAppointments] = useState([]);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="./vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    const createNewAppointment = () => {
+        setAppointments([...appointments, {
+            location: 'San Diego',
+            date: new Date(),
+            description: 'This is a test'
+        }])
+    }
+
+    return (
+        <div className="App">
+            <h1>Movemedical Appointment Scheduler</h1>
+            <button onClick={createNewAppointment}>Create New Appointment</button>
+            <div className="appointment-list">
+                {appointments?.map((appointment, key) => 
+                    <a className="appointment" key={key}>{appointment?.location}</a>
+                )}
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
